c21704f2b13f8a90d89656ff99dcef80f65749e4
DyerMandujano <dyer0206@gmail.com>
2024-06-25 16:43:56 -0500

    primer commit

---
+++ Pry_Banco_Grupo4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pry_Banco_Grupo4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
