b935d6d564aa9fd09c44d03cedf53d77fff55f35
DyerMandujano <dyer0206@gmail.com>
2024-06-25 16:47:59 -0500

    creando clase cuenta

---
+++ Pry_Banco_Grupo4/CuentaBanco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pry_Banco_Grupo4
+{
+    class CuentaBanco
+    {
+        private int num_cliente;
+        private string nombre_cliente;
+        private int antig;
+        private int num_cuenta;
+        private string tipo_cuenta;
+        private string moneda;
+        private double saldo;
+        private int token_dg;
+
+    }
+}
