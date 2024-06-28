bca5bf85040a73a9300488662c0e7efc262ff392
DyerMandujano <dyer0206@gmail.com>
2024-06-28 11:43:25 -0500

    arreglando program

--- Pry_Banco_Grupo4/Program.cs
+++ Pry_Banco_Grupo4/Program.cs
@@ -10,6 +10,7 @@ namespace Pry_Banco_Grupo4
     internal class Program
     {
         public static CuentaCola Nodoctas = new CuentaCola();
+        public static int opc;
 
         static void Main(string[] args)
         {
@@ -19,52 +20,60 @@ namespace Pry_Banco_Grupo4
 
         public static void PantallaPrincipal()
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
             string[] validar = { "1", "2", "3", "4" };
-            while (true)
+
+            do
             {
-                Console.Clear();
-                Console.WriteLine(" ==================================================");
-                Console.WriteLine("\n\t    MENU PRINCIPAL");
-                Console.WriteLine("\n\t[1] Ingreso Cuenta");
-                Console.WriteLine("\t[2] Eliminar Cuenta");
-                Console.WriteLine("\t[3] Visualizar Cuentas");
-                Console.WriteLine("\t[4] Salir");
-                Console.WriteLine(" ==================================================");
-
-                string dato = Console.ReadLine();
-                int opcion = validar.Contains(dato) ? int.Parse(dato) : 0;
-                switch (opcion)
+                try
                 {
-                    case 1: // Opción "Ingresar"
-                        IngresoCuenta();
-                        break;
-                    case 2: // Opción "Eliminar"
-                        EliminarCuenta();
-                        break;
-                    case 3: // Opción "Visualizar"
-                        VisualizarCuenta();
-                        break;
-                    case 4:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine(" Opción no válida");
-                        Console.ReadLine();
-                        opcion = 0;
-                        break;
+                    Console.Clear();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ╔════════════════════════════════════════════╗");
+                    Console.WriteLine(" ║              Menu Cuenta Cliente           ║");
+                    Console.WriteLine(" ╠════════════════════════════════════════════╣");
+                    Console.WriteLine(" ║[1]Ingresar Cuenta Cliente                  ║");
+                    Console.WriteLine(" ║[2]Mostrar Cuentas (COLA)                   ║");
+                    Console.WriteLine(" ║[3]Mostrar Cuentas (ARBOL)                  ║");
+                    Console.WriteLine(" ║[4]Eliminar Cuenta de Cliente (ARBOL)       ║");
+                    Console.WriteLine(" ║[5]Buscar Cuenta por IdCliente              ║");
+                    Console.WriteLine(" ║[6]Mostrar Altura del ARBOL                 ║");
+                    Console.WriteLine(" ║[7]Recorrido del ARBOL en PostOrden         ║");
+                    Console.WriteLine(" ║[8]Mostrar Num de Clientes por Antiguedad   ║");
+                    Console.WriteLine(" ║[9]Mostrar Num de Clientes por Cuenta       ║");
+                    Console.WriteLine(" ║[10]Mostrar Num de Clientes por Tipo Moneda ║");
+                    Console.WriteLine(" ║[11]Salir                                   ║");
+                    Console.WriteLine(" ╚════════════════════════════════════════════╝");
+                    Console.Write(" ELIJA UNA OPCION: ");
+                    opc = int.Parse(Console.ReadLine());
+                    switch(opc) 
+                    {
+                        case 1:
+                            IngresoCuenta();
+                            break;
+                        case 2:
+                            VisualizarCuenta();
+                            break;
+                        case 3:
+                            break;
 
+                    }
                 }
-            }
+                catch
+                {
+                    Console.WriteLine("Coloque una opción valida!!.");
+                    Console.ReadKey();
+                }
+            } while (opc != 11);
+
+
         }
         public static void IngresoCuenta()
         {
             string[] validarTipo = { "A", "C" };
             string[] validarMoneda = { "S", "D" };
-            var culture = new CultureInfo("en-US");
             Random _random = new Random();
-
             Console.Clear();
             Console.WriteLine(" =============================================");
             Console.WriteLine("   INGRESO");
@@ -73,12 +82,12 @@ namespace Pry_Banco_Grupo4
             int _numeroCliente = _random.Next(105, 900);
             Console.Write(_numeroCliente);
             Console.Write("\n");
-            Console.Write("Nombre Cliente               : ");
+            Console.Write(" Nombre Cliente              : ");
             string _nombreCliente = Console.ReadLine();
             if (_nombreCliente.Length == 0) return;
             Console.Write("  Antiguedad                 : ");
             string _antiguedad_ = Console.ReadLine();
-            int _antiguedad = int.TryParse(_antiguedad_, out _) ? Int32.Parse(_antiguedad_, culture) : 0;
+            int _antiguedad = int.TryParse(_antiguedad_, out _) ? Int32.Parse(_antiguedad_) : 0;
             if (!(_antiguedad >= 1 && _antiguedad <= 10))
             {
                 Console.WriteLine("Dato incorrecto");
@@ -87,7 +96,7 @@ namespace Pry_Banco_Grupo4
             }
             Console.Write("  Nro.Cuenta                 : ");
             string _nrocuenta_ = Console.ReadLine();
-            int _nrocuenta = int.TryParse(_nrocuenta_, out _) ? Int32.Parse(_nrocuenta_, culture) : 0;
+            int _nrocuenta = int.TryParse(_nrocuenta_, out _) ? Int32.Parse(_nrocuenta_) : 0;
             if (!(_nrocuenta >= 4500 && _nrocuenta <= 9230))
             {
                 Console.WriteLine("Dato incorrecto");
@@ -134,7 +143,7 @@ namespace Pry_Banco_Grupo4
                 Console.ReadLine();
                 return;
             }
-            double _saldo = double.TryParse(_saldo_, out _) ? double.Parse(_saldo_, culture) : 0;
+            double _saldo = double.TryParse(_saldo_, out _) ? double.Parse(_saldo_) : 0;
             if (!(_saldo >= 50 && _saldo <= 15000))
             {
                 Console.WriteLine("Dato incorrecto");
@@ -149,7 +158,7 @@ namespace Pry_Banco_Grupo4
                 Console.ReadLine();
                 return;
             }
-            int _token = int.TryParse(_token_, out _) ? Int32.Parse(_token_, culture) : 0;
+            int _token = int.TryParse(_token_, out _) ? Int32.Parse(_token_) : 0;
             if (!(_token >= 1120 && _token <= 9999))
             {
                 Console.WriteLine("Dato incorrecto");
@@ -183,28 +192,28 @@ namespace Pry_Banco_Grupo4
 
             Console.Clear();
             Console.WriteLine("Cuentas de cliente");
-            Console.WriteLine("╔════════════╦══════════════════════════╦════════════════╦══════════════════╦════════════════╦═══════════╦══════════╦══════════╗");
-            Console.WriteLine("║  NRO       ║                          ║                ║                  ║      TIPO      ║           ║          ║          ║");
-            Console.WriteLine("║  CLIENTE   ║    NOMBRE CLIENTE        ║  ANTIGUEDAD    ║   NRO.CUENTA     ║     CUENTA     ║   MONEDA  ║  SALDO   ║  TOKEN   ║");
+            Console.WriteLine("╔════════════╦══════════════════════════╦════════════════╦══════════════════╦════════════════╦═══════════╦═════════╦═════════╗");
+            Console.WriteLine("║  NRO       ║                          ║                ║                  ║      TIPO      ║           ║         ║         ║");
+            Console.WriteLine("║  CLIENTE   ║    NOMBRE CLIENTE        ║  ANTIGUEDAD    ║   NRO.CUENTA     ║     CUENTA     ║   MONEDA  ║  SALDO  ║  TOKEN  ║");
             if (Nodoctas.Inicio != null)
             {
                 MostrarCuenta(Nodoctas.Inicio);
             }
-            Console.WriteLine("╚════════════╩══════════════════════════╩════════════════╩══════════════════╩════════════════╩═══════════╩══════════╩══════════╝");
+            Console.WriteLine("╚════════════╩══════════════════════════╩════════════════╩══════════════════╩════════════════╩═══════════╩═════════╩═════════╝");
             Console.ReadLine();
 
         }
         public static void MostrarCuenta(CuentaBanco unaCta)
         {
-            Console.WriteLine("╠════════════╬══════════════════════════╬════════════════╬══════════════════╬════════════════╬═══════════╬══════════╬══════════╣");
+            Console.WriteLine("╠════════════╬══════════════════════════╬════════════════╬══════════════════╬════════════════╬═══════════╬═════════╬═════════╣");
             Console.WriteLine("║" + unaCta.NumeroCliente.ToString().PadRight(12, ' ').Substring(0, 12) + "║" +
                               unaCta.NombreCliente.TrimEnd().PadRight(26).Substring(0, 26) + "║" +
                               unaCta.Antiguedad.ToString().PadRight(16, ' ').Substring(0, 16) + "║     " +
                               unaCta.NumeroCuenta.ToString().PadRight(10, ' ').Substring(0, 10) + "   ║" +
                               (unaCta.TipoCuenta == "A" ? "     AHORRO     " : "CUENTA CORRIENTE") + "║   " +
                               (unaCta.Moneda == "S" ? " SOLES  " : "DÓLARES ") + "║" +
-                              unaCta.Saldo + "        ║" +
-                              unaCta.TokenDigital + "      ║");
+                              unaCta.Saldo + "      ║" +
+                              unaCta.TokenDigital + "     ║");
             if (unaCta.Sgte != null)
             {
                 MostrarCuenta(unaCta.Sgte);
